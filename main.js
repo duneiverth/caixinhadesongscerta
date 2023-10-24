22e6ec6eec5256e054aabd3cf8b177fe56da4759
Duda <105284664+duneiverth@users.noreply.github.com>
2023-10-24 09:23:33 -0300

    ddd

--- main.js
+++ main.js
@@ -12,8 +12,9 @@ const listaDeTeclas = document.querySelector('.tecla');
 listaDeTeclas[0].onclick = tocaSomPom;
 
 let contador = 0;
-while(contador < 9){
+while(contador < listaDeTeclas.length){
     listaDeTeclas[contador].onclick = tocaSomPom; 
     contador = contador + 1;
+    console.log(contador);
 
 }
